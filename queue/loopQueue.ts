61b257d227a9875bfc90173849e12a84ba54950f
Shen Tao <1021572312@qq.com>
2020-07-20 10:16:03 +0800

    队列

--- queue/loopQueue.ts
+++ queue/loopQueue.ts
@@ -1,6 +1,8 @@
 export interface Queue<T> {
+  // O(1)
   enqueue(item: T): void
 
+  // O(1)
   dequeue(): T
 
   // O(1)
@@ -35,4 +37,77 @@ class LoopQueue<T> implements Queue<T> {
   isEmpty(): boolean {
     return this.front === this.tail
   }
+
+  enqueue(item: T): void {
+    if (this.size === this.getCapacity()) {
+      this.resize(this.getCapacity() * 2)
+    }
+
+    this.data[this.tail] = item
+    this.tail = (this.tail + 1) % this.data.length
+    this.size++
+  }
+
+  dequeue(): T {
+    if (this.isEmpty()) {
+      throw new Error('C annot dequeue from an empty queue')
+    }
+
+    let ret = this.data[this.front]
+
+    this.data[this.front] = null
+    this.front = (this.front + 1) % this.data.length
+    this.size--
+
+    if (this.size === this.getCapacity() / 4 && this.getCapacity() / 2 !== 0) {
+      this.resize(this.getCapacity() / 2)
+    }
+
+    return ret
+  }
+
+  private resize(newCapacity: number): void {
+    let newData = Array(newCapacity + 1)
+
+    for (let i = 0; i < this.size; i++) {
+      newData[i] = this.data[(i + this.front) % this.data.length]
+    }
+
+    this.data = newData
+    this.front = 0
+    this.tail = this.size
+  }
+
+  getFront(): T {
+    if (this.isEmpty()) {
+      throw new Error('The enqueue is empty')
+    }
+
+    return this.data[this.front]
+  }
+
+  print() {
+    let str = 'front ['
+
+    for (let i = this.front; i !== this.tail; i = (i + 1) % this.data.length) {
+      str += this.data[i]
+      if ((i + 1) % this.data.length !== this.tail) {
+        str += ','
+      }
+    }
+
+    str += '] tail'
+
+    return str
+  }
 }
+
+const loop1 = new LoopQueue(3)
+for (let i = 0; i < 10; i++) {
+  loop1.enqueue(i)
+  if (i % 3 === 2) {
+    loop1.dequeue()
+  }
+}
+loop1
+console.log(loop1.print())
