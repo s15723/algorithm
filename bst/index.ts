60171b5c25de182498307bab2717878e404f7f72
shentao <1021572312@qq.com>
2020-08-05 09:15:03 +0800

    二分搜索树遍历

--- bst/index.ts
+++ bst/index.ts
@@ -53,12 +53,94 @@ export default class BST<T> {
             return this._contains(node.right, item)
         }
     }
+
+    // 前序遍历
+    preOrder() {
+        this._preOrder(this.root)
+    }
+
+    // 前序遍历以 node 为根的二分搜索树
+    private _preOrder(node: BstNode<T>) {
+        if (node === null) {
+            return
+        }
+
+        console.log(node.val)
+        this._preOrder(node.left)
+        this._preOrder(node.right)
+    }
+
+    // 中序遍历
+    inOrder() {
+        this._inOrder(this.root)
+    }
+
+    private _inOrder(node: BstNode<T>) {
+        if (node === null) {
+            return
+        }
+        // 遍历到叶子节点，return
+        this._inOrder(node.left)
+        console.log(node.val)
+        this._inOrder(node.right)
+    }
+
+    // 后序遍历
+    postOrder() {
+        this._postOrder(this.root)
+    }
+
+    private _postOrder(node: BstNode<T>) {
+        if (node === null) {
+            return
+        }
+        this._postOrder(node.left)
+        this._postOrder(node.right)
+        console.log(node.val)
+    }
+
+    toString() {
+        let res = ''
+        res = this.generateBSTString(this.root, 0, res)
+        return res
+    }
+
+    private generateBSTString(
+        node: BstNode<T>,
+        depth: number,
+        res: string
+    ): string {
+        if (node === null) {
+            res += `${this.generateDepthString(depth)}null\n`
+            return res
+        }
+
+        res += `${this.generateDepthString(depth)}${node.val}\n`
+        res = this.generateBSTString(node.left, depth + 1, res)
+        res = this.generateBSTString(node.right, depth + 1, res)
+
+        return res
+    }
+
+    private generateDepthString(depth: number) {
+        let res = ''
+        for (let i = 0; i < depth; i++) {
+            res += '--'
+        }
+        return res
+    }
 }
 
 let bst1 = new BST<number>()
-for (let i = 0; i < 10; i++) {
-    bst1.add(i)
+const nums = [5, 3, 6, 8, 4, 2]
+for (let i = 0; i < nums.length; i++) {
+    bst1.add(nums[i])
 }
-console.log(bst1)
-console.log(bst1.contains(5))
-console.log(bst1.contains(11))
+
+// console.log(bst1)
+// console.log(bst1.contains(5))
+// console.log(bst1.contains(11))
+// bst1.preOrder()
+console.log('toString\n', bst1.toString())
+// bst1.inOrder()
+// bst1.postOrder()
