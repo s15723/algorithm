dc83c5c46124ce4fb1e92427b1348be8801efc44
shentao <1021572312@qq.com>
2020-09-08 02:52:09 +0800

    feat: 优化快速排序

--- packages/sort/quickSort/index.ts
+++ packages/sort/quickSort/index.ts
@@ -1,7 +1,9 @@
 /**
  * 快速排序基础版 O(nlogn)
  *
- * 对于近乎有序的数组, 快速排序算法退化成了O(n^2)级别的算法，因为有序数组也要不停地 swap 吧？
+ * 对于近乎有序的数组, 快速排序算法退化成了O(n^2)级别的算法，会退化成一个长度为 n 的链表，每层处理 n 次
+ * 可以通过 partition 里的优化 2 来解决，防止对于有序数组，一直取最左边的最小值导致树退化成链表
+ * 
  */
 export default function quickSort<T>(arr: T[]) {
     const n = arr.length
@@ -24,6 +26,9 @@ function _quickSort<T>(arr: T[], left: number, right: number) {
 // 返回 p，使得 arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
 // 即找到原数组第一个元素排完序后应该在的位置
 function partition<T>(arr: T[], left: number, right: number): number {
+    // 优化 2
+    // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+    swap(arr, left, Math.floor(Math.random() * (right - left + 1) + left))
     const v = arr[left]
 
     // arr[l+1, j] < v, arr[j+1, i) > v
