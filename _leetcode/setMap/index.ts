2d92b289ef7c93575563e6f718055b467f447be8
shentao <1021572312@qq.com>
2020-10-14 00:05:22 +0800

    feat: 交集

---
+++ _leetcode/setMap/index.ts
@@ -0,0 +1,27 @@
+/**
+ * 有序数组的交集,重复元素交集算多个
+ * O(k+j),k,j分别为num1,nums2的长度
+ */
+function intersection(nums1: number[], nums2: number[]): number[] {
+    const n1 = nums1.length,
+        n2 = nums2.length
+    let i = 0,
+        j = 0,
+        res: number[] = []
+    while (i < n1 && j < n2) {
+        if (nums1[i] === nums2[j]) {
+            res.push(nums1[i])
+            i++
+            j++
+        } else if (nums1[i] < nums2[i]) {
+            i++
+        } else {
+            j++
+        }
+    }
+
+    return res
+}
+let nums1 = [1, 1, 2, 2],
+    nums2 = [2, 2, 3, 4]
+console.log(intersection(nums1, nums2))
