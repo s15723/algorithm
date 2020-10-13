91b6def253afab989080f74972bc4b6e122b645b
Shen Tao <1021572312@qq.com>
2020-10-13 11:30:39 +0800

    查找表: 快慢指针龟兔赛跑202

--- _leetcode/setAndMap/202/index.ts
+++ _leetcode/setAndMap/202/index.ts
@@ -1,10 +1,10 @@
 /**
  * 快乐数
  * https://coding.imooc.com/lesson/82.html#mid=2666
- * 
- * 时间 O(logn)
- * 空间 O(logn)
  */
+// 解法 1
+// 时间 O(logn)
+// 空间 O(logn)
 function isHappy(n: number): boolean {
     const record = new Set<number>()
     record.add(n)
@@ -31,3 +31,33 @@ function calc(n: number) {
 
     return res
 }
+
+/**
+ * 解法 2
+ * 时间 O(logn)
+ * 空间 O(1)
+ * 我们不是只跟踪链表中的一个值，而是跟踪两个值，称为快跑者和慢跑者。在算法的每一步中，慢速在链表中前进 1 个节点，快跑者前进 2 个节点（对 getNext(n) 函数的嵌套调用）。
+ * 如果 n 是一个快乐数，即没有循环，那么快跑者最终会比慢跑者先到达数字 1。
+ * 如果 n 不是一个快乐的数字，那么最终快跑者和慢跑者将在同一个数字上相遇。
+ */
+function isHappy2(n: number): boolean {
+    if (n === 1) return true
+
+    let slow = calc(n)
+    if (slow === 1) return true
+    let fast = calc(slow)
+    if (fast === 1) return true
+
+    while (slow !== fast) {
+        slow = calc(slow)
+        fast = calc(fast)
+        if (fast === 1) return true
+        fast = calc(fast)
+        if (fast === 1) return true
+    }
+
+    return false
+}
+
+console.log(isHappy2(2))
+
